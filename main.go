aade782ec389270b682aead6f5fee0656ca9e356
bbcvc <1103787680@qq.com>
2022-10-03 18:50:50 +0800

    chore: gin

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
-func main() {
-	println(sum(1, 2))
-}
+import (
+	"net/http"
 
-func sum(a int, b int) int {
-	return a + b
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
